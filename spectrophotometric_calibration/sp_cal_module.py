49ccc20b194672f2722f4b8f191382eb4c05eb3a
ashindin <shindin@rf.unn.ru>
2017-04-24 17:46:30 +0300

    fix

--- spectrophotometric_calibration/sp_cal_module.py
+++ spectrophotometric_calibration/sp_cal_module.py
@@ -38,7 +38,7 @@ def tan_get_pixel_solid_angle(M,theta):
     Omega=1/A**2*(np.cos(theta)**3)
     return Omega
 
-def get_brightness_in_Rayleighs(M,theta,dlambda,sol_angle,num_of_pixels,flux):
+def get_brightness_in_Rayleighs(dlambda,sol_angle,num_of_pixels,flux):
     const=4*np.pi*6.3*dlambda/6.626/3/(sol_angle*10**6)
 #     print(const)
     R=(const*10**4)*flux/num_of_pixels
@@ -47,12 +47,12 @@ def get_brightness_in_Rayleighs(M,theta,dlambda,sol_angle,num_of_pixels,flux):
 # az0,alt0,a,b,c,d = get_solve_pars("../astrometric_calibration/keo_140824_solve_manual.pars")
 # M_keo = get_scale_and_orientation_info(c,d)
 # sol_angle_keo=arc_get_pixel_solid_angle(M_keo,36*np.pi/180)
-# print(get_brightness_in_Rayleighs(M_keo,36*np.pi/180,20,sol_angle_keo,6,236.5*10**-3))
+# print(get_brightness_in_Rayleighs(M_keo,20,sol_angle_keo,6,236.5*10**-3))
 
 # az0,alt0,a,b,c,d = get_solve_pars("../astrometric_calibration/s1c_140824_solve.pars")
 # M_s1c = get_scale_and_orientation_info(c,d)
 # sol_angle_s1c=tan_get_pixel_solid_angle(M_s1c,5*np.pi/180)
-# get_brightness_in_Rayleighs(M_s1c,5*np.pi/180,100,sol_angle_s1c,12,236.5*10**-3)
+# get_brightness_in_Rayleighs(M_s1c,100,sol_angle_s1c,12,236.5*10**-3)
 
 def load_sp_catalog(cat_fname):
     fid=open(cat_fname,'r')
