0447cae8c88066bbec8c90386ad9e3f1dc5ee0c1
ashindin <shindin@rf.unn.ru>
2017-05-02 08:45:18 +0300

    add script to download sources of processing stack

---
+++ get_sources.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# python 3.6.0
+wget https://www.python.org/ftp/python/3.6.0/Python-3.6.0.tgz
+# numpy 1.11.3
+wget -O numpy-1.11.3.tar.gz https://github.com/numpy/numpy/archive/v1.11.3.tar.gz
+# scipy 0.18.1
+wget -O scipy-0.18.1.tar.gz https://github.com/scipy/scipy/archive/v0.18.1.tar.gz
+# matplotlib 2.0.0
+wget -O matplotlib-2.0.0.tar.gz https://github.com/matplotlib/matplotlib/archive/v2.0.0.tar.gz
+# sympy 1.0
+wget https://github.com/sympy/sympy/archive/sympy-1.0.tar.gz
+# astropy 1.3
+wget -O astropy-1.3.tar.gz https://github.com/astropy/astropy/archive/v1.3.tar.gz
+# astrometry.net 0.70
+wget -O astrometry.net-0.70.tar.gz https://github.com/dstndstn/astrometry.net/archive/0.70.tar.gz
+# ffmpeg 3.2.4
+wget -O ffmpeg-3.2.4.tar.gz https://github.com/FFmpeg/FFmpeg/archive/n3.2.4.tar.gz
