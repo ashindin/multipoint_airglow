a4be5e26b8d9980cdf8e4fcdf866450468abdee8
ashindin <shindin@rf.unn.ru>
2017-06-25 18:30:05 +0300

    add file

--- modelling/sphere_model_gen.py
+++ modelling/sphere_model_gen.py
@@ -3,6 +3,7 @@ from sympy import *
 
 fun_str='a1*exp(-(x**2 + y**2 + z**2) /a2**2)'
 model_fname='sphere.dat'
+model_fun_fname='model_sphere_fun.py'
 
 fun=sympify(fun_str)
 
@@ -17,6 +18,7 @@ phi_k, lam_k, h_k = symbols('phi_k lam_k h_k');
 a1, a2 = symbols('a1 a2');
 
 coord_replacement_fn='coord_sys_replacement.dat'
+
 
 fid=open(coord_replacement_fn,'r')
 fid.readline()
@@ -36,4 +38,17 @@ funREA=fun.subs([(x,x_fun),
 fid=open(model_fname,'w')
 fid.write('# sphere model function to integrate\n')
 fid.write(str(funREA)+'\n')
+fid.close()
+
+funREA_str=str(funREA)
+funREA_numpy=funREA_str.replace('exp','np.exp').replace('sin','np.sin').replace('cos','np.cos')
+
+fid=open(model_fun_fname,'w')
+fid.write('import numpy as np\n')
+fid.write('def sphere_fun(R,E,A,a,mod_pos,cam_pos):\n')
+fid.write('    a1=aa[0]\n    a2=aa[1]\n')
+fid.write('    phi_m=mod_pos[0]\n    lam_m=mod_pos[1]\n    h_m=mod_pos[2]\n')
+fid.write('    phi_k=cam_pos[0]\n    lam_m=cam_pos[1]\n    h_m=cam_pos[2]\n')
+fid.write('    a=6378137.0\n    b=6356752.314245\n    e=np.sqrt(1-b**2/a**2)\n')
+fid.write('    return ' + funREA_numpy + '\n')
 fid.close()
