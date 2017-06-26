110d78e8cb0cf627aa84740d0da3c4b957b891c6
ashindin <shindin@rf.unn.ru>
2017-06-26 16:55:45 +0300

    add all models

--- modelling/beta_model_gen.py
+++ modelling/beta_model_gen.py
@@ -1,9 +1,9 @@
 from sympy import *
+
 # init_printing()
 
-fun_str='a1*exp(-(x**2 + y**2 + z**2) /a2**2)'
-#model_fname='sphere.dat'
-model_fun_fname='model_sphere_fun.py'
+fun_str='a1*exp(-(x/a2)**2 - (y/a3)**2)*1/a4/beta(a5,a6)*((z-h_m)/a4)**(a5-1)*((a4-z+h_m)/a4)**(a6-1)'
+model_fun_fname='model_beta_fun.py'
 
 fun=sympify(fun_str)
 
@@ -15,7 +15,7 @@ R, E, A = symbols('R E A')
 phi, lam, h = symbols('phi lam h');
 phi_m, lam_m, h_m = symbols('phi_m lam_m h_m');
 phi_k, lam_k, h_k = symbols('phi_k lam_k h_k');
-a1, a2 = symbols('a1 a2');
+a1, a2, a3, a4, a5, a6, a7, a8, a9 = symbols('a1 a2 a3 a4 a5 a6 a7 a8 a9');
 
 coord_replacement_fn='coord_sys_replacement.dat'
 
@@ -40,14 +40,16 @@ funREA=fun.subs([(x,x_fun),
 #fid.write(str(funREA)+'\n')
 #fid.close()
 
-funREA_str=str(funREA)
+funREA_str=str(funREA).replace('beta(a5, a6)','beta_a5a6')
 #funREA_numpy=funREA_str.replace('exp','np.exp').replace('sin','np.sin').replace('cos','np.cos').replace('sqrt','np.sqrt')
 
 fid=open(model_fun_fname,'w')
 #fid.write('import numpy as np\n')
 fid.write('import numexpr as ne\n')
+fid.write('import scipy.special as sp\n')
 fid.write('def sphere_fun(R,E,A,aa,mod_pos,cam_pos):\n')
-fid.write('    a1=aa[0]\n    a2=aa[1]\n')
+fid.write('    a1=aa[0]\n    a2=aa[1]\n    a3=aa[2]\n    a4=aa[3]\n    a5=aa[4]\n    a6=aa[5]\n')
+fid.write('    beta_a5a6=sp.beta(a5,a6)\n')
 fid.write('    phi_m=mod_pos[0]\n    lam_m=mod_pos[1]\n    h_m=mod_pos[2]\n')
 fid.write('    phi_k=cam_pos[0]\n    lam_k=cam_pos[1]\n    h_k=cam_pos[2]\n')
 fid.write('    a=6378137.0\n    b=6356752.314245\n    e=0.081819190842965558\n')
--- modelling/ellipsoid_model_gen.py
+++ modelling/ellipsoid_model_gen.py
@@ -1,9 +1,8 @@
 from sympy import *
 # init_printing()
 
-fun_str='a1*exp(-(x**2 + y**2 + z**2) /a2**2)'
-#model_fname='sphere.dat'
-model_fun_fname='model_sphere_fun.py'
+fun_str='a1*exp(-((x/a2)**2 + (y/a3)**2 + (z/a4)**2))'
+model_fun_fname='model_ellipsoid_fun.py'
 
 fun=sympify(fun_str)
 
@@ -15,7 +14,7 @@ R, E, A = symbols('R E A')
 phi, lam, h = symbols('phi lam h');
 phi_m, lam_m, h_m = symbols('phi_m lam_m h_m');
 phi_k, lam_k, h_k = symbols('phi_k lam_k h_k');
-a1, a2 = symbols('a1 a2');
+a1, a2, a3, a4, a5, a6, a7, a8, a9 = symbols('a1 a2 a3 a4 a5 a6 a7 a8 a9');
 
 coord_replacement_fn='coord_sys_replacement.dat'
 
@@ -47,7 +46,7 @@ fid=open(model_fun_fname,'w')
 #fid.write('import numpy as np\n')
 fid.write('import numexpr as ne\n')
 fid.write('def sphere_fun(R,E,A,aa,mod_pos,cam_pos):\n')
-fid.write('    a1=aa[0]\n    a2=aa[1]\n')
+fid.write('    a1=aa[0]\n    a2=aa[1]\n    a3=aa[2]\n    a4=aa[3]\n')
 fid.write('    phi_m=mod_pos[0]\n    lam_m=mod_pos[1]\n    h_m=mod_pos[2]\n')
 fid.write('    phi_k=cam_pos[0]\n    lam_k=cam_pos[1]\n    h_k=cam_pos[2]\n')
 fid.write('    a=6378137.0\n    b=6356752.314245\n    e=0.081819190842965558\n')
